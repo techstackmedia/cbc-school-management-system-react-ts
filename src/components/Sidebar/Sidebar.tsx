cce496c4733ec3ca81624909807359882ebfc20d
techstackmedia <osagienoah04@gmail.com>
2022-03-28 06:39:05 +0100

    commit changes

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -4,28 +4,19 @@ import logo from "../../images/logo.jpeg";
 import arrowSide from "../../images/arrowside.svg";
 
 const Sidebar = () => {
-  const sidebarItems = [
-    "dashboard",
-    "user management",
-    "schools",
-    "teachers",
-    "students",
-    "reports",
-    "school",
-    "teacher",
-    "student",
-    "attendance",
-    "student attendance",
-    "teacher attendance",
+  const sidebarNavItems = [
+    "Dashboard",
+    "User Management",
+    "Schools",
+    "Teachers",
+    "Students",
   ];
 
-  const sidebarItemsElem = sidebarItems.map((item, index) => {
+  const sidebarNavItemsElem = sidebarNavItems.map((item, index) => {
     return (
       <>
         <li className={sidebarStyle[`item${index}`]} key={index}>
-          <Link
-            to={item.replace(" ", "-")}
-          >{`${item[0].toUpperCase()}${item.slice(1)}`}</Link>
+          <Link to={item.toLowerCase().replace(" ", "-")}>{item}</Link>
         </li>
         <Outlet />
       </>
@@ -43,7 +34,68 @@ const Sidebar = () => {
         </div>
       </div>
       <nav className={sidebarStyle.navSidebar}>
-        <ul>{sidebarItemsElem}</ul>
+        <ul>{sidebarNavItemsElem}</ul>
+        <div>REPORTS</div>
+        <ul>
+          <li>
+            School
+            <ul>
+              <li>
+                <Link to="/school/school-distribution-by-state">
+                  School Distribution by State
+                </Link>
+              </li>
+              <li>
+                <Link to="/school/school-distribution-by-lga">
+                  School Distribution by LGA
+                </Link>
+              </li>
+            </ul>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            Teacher
+            <ul>
+              <li>
+                <Link to="/teacher/teachers-qualification">
+                  Teachers Qualification
+                </Link>
+              </li>
+              <li>
+                <Link to="/teacher/qualification-by-school">
+                  Qualification By School
+                </Link>
+              </li>
+              <li>
+                <Link to="/teacher/subject-distribution">
+                  Subject Distribution
+                </Link>
+              </li>
+              <li>
+                <Link to="effective-report">
+                  Effective Report
+                  <ul>
+                    <li>
+                      <Link to="/teacher/effective-report/state-report">
+                        State Report
+                      </Link>
+                    </li>
+                  </ul>
+                </Link>
+              </li>
+            </ul>
+          </li>
+        </ul>
+        <div>ATTENDANCE</div>
+        <ul>
+          <li>
+            <Link to="student-attendance">Student Attendance</Link>
+          </li>
+          <li>
+            <Link to="teacher-attendance">Teacher Attendance</Link>
+          </li>
+        </ul>
       </nav>
     </aside>
   );
